e4be56754c671cfe97a4a62e72be069bad39c4b9
18langbob <18langbob@gmail.com>
2026-06-19 10:56:30 +0100

    Finished 2nd Test Set
    Finished 2nd stage of strategy testing with increased sample sizes and more specific values tested.

--- Flip 7 Test Environment.py
+++ Flip 7 Test Environment.py
@@ -38,6 +38,9 @@ currentHand = []
 heuristicsData = [] 
 strategyData = []
 
+tempList = []
+tempString = ""
+
 choice = ""
 score = 0
 roundNum = 1
@@ -58,14 +61,14 @@ def FlipCard():
     if masterList[0] == "Unlucky 7":
         currentHand = ["Unlucky 7"]
         cumulativeProb = 1.0
-        print("Bad Luck! Hand Reset due to Unlucky 7!")
+        #print("Bad Luck! Hand Reset due to Unlucky 7!")
     else:
         currentHand.append(masterList[0])
         for i in range(0,len(gameCards)):
             if masterList[0] == gameCards[i][0] :
                 gameCards[i][2] -= 1
     masterList.remove(masterList[0])
-    print("Your current hand is: " + str(currentHand))
+    #print("Your current hand is: " + str(currentHand))
    
 def CheckFlip7():
     global currentHand
@@ -74,7 +77,7 @@ def CheckFlip7():
         if currentHand[i].isnumeric() or currentHand[i] == "The 0" or currentHand[i] == "Unlucky 7" or currentHand[i] == "Lucky 13":
             tempHand.append(currentHand[i])
     if len(tempHand) == 7 :
-        print("Flipped 7!")
+        #print("Flipped 7!")
         return True
    
 def CheckBust():
@@ -82,7 +85,7 @@ def CheckBust():
     tempHand = []
     for i in range(0,len(currentHand)):
         if currentHand[i] in tempHand or ((currentHand[i] == "7") and ("Unlucky 7" in tempHand)) or ((currentHand[i] == "Unlucky 7") and ("7" in tempHand)):
-            print("You Busted!")
+            #print("You Busted!")
             return True
         else:
             tempHand.append(currentHand[i])
@@ -98,8 +101,8 @@ def CheckHand():
 def ResetHand():
     global currentHand
     currentHand = []
-    print("")
-    print("Round " + str(roundNum))
+    #print("")
+    #print("Round " + str(roundNum))
     
 def ReshuffleDeck(pHand):
     global gameCards
@@ -128,16 +131,17 @@ def Bank(pBonus=0):
             temp0Flag = True
     if temp0Flag :
         if pBonus == 0 :
-            print("Banked Nothing!")
+            pass
+            #print("Banked Nothing!")
         else:
             score += addToScore
             score += pBonus
-            print("You scored " + str(addToScore+pBonus)+" for this round!")
+            #print("You scored " + str(addToScore+pBonus)+" for this round!")
     else:
         score += addToScore
         score += pBonus
-        print("You scored " + str(addToScore+pBonus)+" for this round!")
-    print("Your total score is: " + str(score))
+        #print("You scored " + str(addToScore+pBonus)+" for this round!")
+    #print("Your total score is: " + str(score))
     
 def ShowProbBust():
     global currentHand
@@ -202,70 +206,92 @@ def Guesstimator(pHandScore):
     estimation -= (roundNum - 1) * adjustmentConstant
     return(estimation)
 
-print("Round " + str(roundNum))
-while score < 200:
-    
-    #print(ShowProbBust()*100)
-    print(Guesstimator(CheckHandScore(1)))
-    #choice = input("Please Select: Flip,Bank,ProbBust,ReHand,ReDeal,CheckScore,CheckHand")
-    if CheckHandScore() < 27:
-        choice = "Flip"
-    else:
-        choice = "Bank"
-    if choice == "Flip":
-        if len(masterList) == 0 :
-            ReshuffleDeck(currentHand)
-            print("Deck Reshuffled!")
-            roundNum = 1
-        #print(ShowProbBust()*100)
-        FlipCard()
-        cumulativeProb *= 1 - ShowProbBust()
+#print("Round " + str(roundNum))
+while count < 100:
+    while score < 200:
         
-        if CheckBust() :
-            roundNum += 1
-            ResetHand()
-            cumulativeProb = 1.0
-            strategyData.append(0)
-        if CheckFlip7():
+        #print(ShowProbBust()*100)
+        #print(Guesstimator(CheckHandScore(1)))
+        #choice = input("Please Select: Flip,Bank,ProbBust,ReHand,ReDeal,CheckScore,CheckHand")
+        if CheckHandScore() < 25:
+            choice = "Flip"
+        else:
+            choice = "Bank"
+        if choice == "Flip":
+            if len(masterList) == 0 :
+                ReshuffleDeck(currentHand)
+                #print("Deck Reshuffled!")
+                #roundNum = 1
+            #print(ShowProbBust()*100)
+            FlipCard()
+            cumulativeProb *= 1 - ShowProbBust()
+            
+            if CheckBust() :
+                roundNum += 1
+                ResetHand()
+                cumulativeProb = 1.0
+                strategyData.append(0)
+            if CheckFlip7():
+                roundNum += 1
+                strategyData.append(CheckHandScore())
+                Bank(15)
+                ResetHand()
+                cumulativeProb = 1.0
+            #if len(currentHand) != 0   :
+                #print("Your guesstimated chance of busting are: " + str(CheckHandScore()) + "%")
+                #print("Your actual chances of busting are: " + str(ShowProbBust()*100) + "%")
+                #print("Your old differential is: " + str(CheckHandScore(1) - (ShowProbBust()*100)) + "%")
+                #print("My new guestimation is " + str(Guesstimator(CheckHandScore(1))))
+                #print("Your new differential is: " + str(Guesstimator(CheckHandScore(1)) - (ShowProbBust()*100)) + "%")
+                #heuristicsData.append(Guesstimator(CheckHandScore(1)) - (ShowProbBust()*100))
+                
+            #else:
+            #    roundNum += 1
+            #    Bank()
+            #    ResetHand()
+            #    cumulativeProb = 1.0
+           
+        elif choice == "Bank":
             roundNum += 1
             strategyData.append(CheckHandScore())
-            Bank(15)
+            Bank()
             ResetHand()
             cumulativeProb = 1.0
-        #if len(currentHand) != 0   :
-            #print("Your guesstimated chance of busting are: " + str(CheckHandScore()) + "%")
-            #print("Your actual chances of busting are: " + str(ShowProbBust()*100) + "%")
-            #print("Your old differential is: " + str(CheckHandScore(1) - (ShowProbBust()*100)) + "%")
-            #print("My new guestimation is " + str(Guesstimator(CheckHandScore(1))))
-            #print("Your new differential is: " + str(Guesstimator(CheckHandScore(1)) - (ShowProbBust()*100)) + "%")
-            #heuristicsData.append(Guesstimator(CheckHandScore(1)) - (ShowProbBust()*100))
-            
-        #else:
-        #    roundNum += 1
-        #    Bank()
-        #    ResetHand()
-        #    cumulativeProb = 1.0
-       
-    elif choice =="Bank":
-        roundNum += 1
-        strategyData.append(CheckHandScore())
-        Bank()
-        ResetHand()
-        cumulativeProb = 1.0
-    #Debug Game Options
-    elif choice == "ProbBust":
-        ShowProbBust()
-    elif choice == "ReHand":
-        ResetHand()
-    elif choice == "ReDeal":
-        ReshuffleDeck(currentHand)
-    elif choice == "CheckScore" :
-        CheckScore()
-    elif choice == "CheckHand" :
-        CheckHand()
-    elif choice == "ShowData" :
-        for i in range(0,len(heuristicsData)):
-            print(heuristicsData[i])
-            
-for i in range(0,len(strategyData)) :   
-    print(strategyData[i])
+        #Debug Game Options
+        elif choice == "ProbBust":
+            ShowProbBust()
+        elif choice == "ReHand":
+            ResetHand()
+        elif choice == "ReDeal":
+            ReshuffleDeck(currentHand)
+        elif choice == "CheckScore" :
+            CheckScore()
+        elif choice == "CheckHand" :
+            CheckHand()
+        elif choice == "ShowData" :
+            for i in range(0,len(heuristicsData)):
+                print(heuristicsData[i])
+     
+    tempString = ""
+    for i in range(0,len(strategyData)) :
+        tempString = tempString + str(strategyData[i]) + " "
+    tempList.append(tempString)
+    
+    strategyData = []
+    #tempString = []
+    
+    score = 0
+    count += 1
+    roundNum = 1
+
+
+tempTotal = 0
+tempRoundCount = 0
+tempBustCount = 0
+tempSuccessCount = 0
+tempAverageScore = 0
+
+tempResultsList = []
+tempResultsSubList = []
+for i in range(0,len(tempList)):
+    print(tempList[i])
